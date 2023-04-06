24f80f32bd47d1730635c7053ddecf3184644725
sailajalakkakula <123012535+sailajalakkakula@users.noreply.github.com>
2023-04-05 21:56:30 -0500

    Added all code changes in repo

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 First page of pug
 
 [Repo link to render](https://s23wb42lakkakula.onrender.com)
+
+1.Ball_Type(String)
+
+2.Ball_Weight(String)
+
+3.Ball_Cost(Number)
--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var BallRouter=require('./routes/Ball')
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -20,7 +21,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+app.use('/ball', BallRouter);
+app.use('/board', boardRouter);
+app.use('/selector',selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
